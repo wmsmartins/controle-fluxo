79b4f1ef7beec1d1519a63b3837d37687819d69a
Wagner Martins <wms76512@gmail.com>
2024-05-24 15:19:25 -0300

    novo exercicio

---
+++ src/ExemploWhile.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.ThreadLocalRandom;
+public class ExemploWhile {
+    public static void main(String[] args) {
+        double mesada = 50.0;
+
+        while( mesada > 0) {
+            double valorDoce = valorAleatorio();
+
+            if (valorDoce > mesada)
+                valorDoce = mesada;
+
+            System.out.println("Doce do valor: " + valorDoce + "Adicionando no carrinho"); 
+            mesada = mesada - valorDoce;   
+        }
+        System.out.println("Mesada:" + mesada);
+        System.out.println("Joãozinho gastou toda a sua mesada em doce");
+        /*
+         * Não se preocupe quanto a formatação de valores
+         * Iremos explorar os recursos de formatação em breve !!
+         */
+        }
+
+        private static double valorAleatorio() {
+            return ThreadLocalRandom.current().nextDouble(2, 15);
+        }
+    }
+
